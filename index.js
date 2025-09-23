a03874de954798321733f0ad375a21f48a71bf20
Eva254-ke <evanszachariah36@gmail.com>
2025-09-23 10:32:58 +0300

    Allow origin headers

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', origin);
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+  res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization,apikey');
   }
   if (req.method === 'OPTIONS') {
     return res.sendStatus(204);
